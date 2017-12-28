c14e0f7e15cf93c5e03ed0119f22e5ed1c458104
SuperDanielLiang <1406859565@qq.com>
2017-12-28 16:34:59 +0800

    test

---
+++ src/test/RegisterTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Register Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>十二月 26, 2017</pre> 
+* @version 1.0 
+*/ 
+public class RegisterTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: doGet(HttpServletRequest req, HttpServletResponse resp) 
+* 
+*/ 
+@Test
+public void testDoGet() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: doPost(HttpServletRequest req, HttpServletResponse resp) 
+* 
+*/ 
+@Test
+public void testDoPost() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/user/UserTest.java
@@ -0,0 +1,165 @@
+package test.user; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* User Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>十二月 26, 2017</pre> 
+* @version 1.0 
+*/ 
+public class UserTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getNickname() 
+* 
+*/ 
+@Test
+public void testGetNickname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPw() 
+* 
+*/ 
+@Test
+public void testGetPw() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUserAccount() 
+* 
+*/ 
+@Test
+public void testGetUserAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUserId() 
+* 
+*/ 
+@Test
+public void testGetUserId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPlayGameCurrentPage() 
+* 
+*/ 
+@Test
+public void testGetPlayGameCurrentPage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTableId() 
+* 
+*/ 
+@Test
+public void testGetTableId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPositionId() 
+* 
+*/ 
+@Test
+public void testGetPositionId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPositionId(int positionId) 
+* 
+*/ 
+@Test
+public void testSetPositionId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setNickname(String nickname) 
+* 
+*/ 
+@Test
+public void testSetNickname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPw(String pw) 
+* 
+*/ 
+@Test
+public void testSetPw() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setUserAccount(String userAccount) 
+* 
+*/ 
+@Test
+public void testSetUserAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setUserId(String userId) 
+* 
+*/ 
+@Test
+public void testSetUserId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPlayGameCurrentPage(int playGameCurrentPage) 
+* 
+*/ 
+@Test
+public void testSetPlayGameCurrentPage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setTableId(int tableId) 
+* 
+*/ 
+@Test
+public void testSetTableId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
